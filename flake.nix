9013f54cf92b9f876ebdc82751b1fcf0fad74557
Nikolay Yakimov <root@livid.pp.ru>
2025-06-19 03:02:11 +0300

    Add flake

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Tweakpoint -- a small daemon tweaking pointer device behaviour";
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachSystem [flake-utils.lib.system.x86_64-linux flake-utils.lib.system.aarch64-linux] (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
+      in
+      with pkgs;
+      {
+        inherit pkgs;
+        devShells.default = mkShell {
+          buildInputs = [
+            rustc
+            cargo
+            clippy
+            rust-analyzer
+            rustfmt
+          ];
+          # Environment variables
+          RUST_SRC_PATH = rustPlatform.rustLibSrc;
+        };
+        packages.default = rustPlatform.buildRustPackage {
+          pname = manifest.name;
+          version = manifest.version;
+          src = lib.cleanSource ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+        };
+        apps.default = flake-utils.lib.mkApp {
+          drv = self.packages.${system}.default;
+        };
+      }
+    );
+}
