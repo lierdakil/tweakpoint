15ec35b8b89436a1687bfb7a65e4f7e79a539c13
Nikolay Yakimov <root@livid.pp.ru>
2025-06-28 21:53:33 +0300

    Be more lenient to socket errors

--- src/main.rs
+++ src/main.rs
@@ -155,10 +155,20 @@ async fn handle_socket(
     socket: tokio::net::UnixListener,
     state_vec_rx: tokio::sync::watch::Receiver<Vec<u8>>,
 ) {
+    use std::time::Instant;
+    let mut limit = (Instant::now(), 0u8);
     loop {
         let Ok((mut conn, _)) = socket.accept().await else {
-            tracing::error!("Failed to accept socket connection; bailing, socket is disabled");
-            break;
+            tracing::error!("Failed to accept socket connection");
+            if limit.1 > 10 && limit.0.elapsed().as_secs_f32() < 5.0 {
+                tracing::error!("Socket connections failing too often; bailing, socket is disabled");
+                break;
+            } else if limit.1 == 0 || limit.0.elapsed().as_secs_f32() >= 5.0 {
+                limit = (Instant::now(), 1);
+            } else { // limit <= 10 elapsed < 5.0
+                limit.1 = limit.1.saturating_add(1);
+            }
+            continue;
         };
         let mut state_vec_rx = state_vec_rx.clone();
         tokio::spawn(async move {
